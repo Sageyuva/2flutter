161156059b5c3726f5b40e304863c28fed1f2dc9
Sageyuva <yuvarajdhammure@gmail.com>
2022-08-24 12:05:52 +0530

    day2 complete

---
+++ lib/pictures/uploads.dart
@@ -0,0 +1,16 @@
+// class catalogmodel{
+//  static final items=[
+// Item(id: 01,
+//  name: "Yuvaraj",
+//   number:79751396915,
+//    image: "https://i.pinimg.com/564x/a3/a4/96/a3a49612f19861a07fcbfdfe1a73cea3.jpg" )
+// ];
+// }
+// class Item{
+//   final num id;
+//   final String name;
+//   final num number;
+//   final String image;
+
+//   Item( { required this.id,required this.name, required this.number,required this.image});
+// }
---
+++ lib/widgets/item_widgets.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_2/pictures/uploads.dart';
+
+// class ItemWidget extends StatelessWidget {
+//   const ItemWidget({Key? key, required this.items, required Item item}) : assert(items!=null);
+
+//   @override
+//   final Item items;
+//   Widget build(BuildContext context) {
+//    return Card(
+//      child: ListTile(
+//       leading: Image.network(items.image),
+//       title: Text(items.name),
+   
+//      ),
+//    ); 
+//   }
+// }
